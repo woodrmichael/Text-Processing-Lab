9dac57fb873c6249c518b2802ded28b41aa72deb
woodrmichael <woodm@msoe.edu>
2023-12-02 21:31:00 -0600

    normalize() working

--- src/woodm/Driver.java
+++ src/woodm/Driver.java
@@ -5,6 +5,11 @@
  */
 package woodm;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -55,7 +60,61 @@ public class Driver {
         return in.nextLine();
     }
 
+    /**
+     * A helper method to remove the header information from a Project Gutenberg file.
+     * The method will continue to consume the buffer of the Scanner until
+     * the header text has been removed, then will stop.
+     * @param read Scanner using a Project Gutenberg text file as input
+     */
+    private static void removeHeader(Scanner read) { // BROKEN
+        try (PrintWriter writer = new PrintWriter("test.txt")) {
+            ArrayList<String> list = new ArrayList<>();
+            read.nextLine();
+            while(read.hasNextLine()) {
+                list.add(read.nextLine());
+            }
+            //read.close();
+            for(int i = 0; i < list.size(); i++) {
+                writer.println(list.get(i));
+            }
+            writer.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println("FILE NOT FOUND");
+        }
+    }
+
+    /**
+     * A helper method that reads words from a text file one at a time and stores the
+     * normalized words in a List of BasicWords.
+     * Any word that contains only whitespace should be ignored.
+     * @param words the List used to store the words
+     * @param read Scanner using a Project Gutenberg text file as input
+     */
+    private static void addWords(List<BasicWord> words, Scanner read) {
+
+    }
+
+    /**
+     * A helper method that removes all punctuation from a String and converts the resulting
+     * punctuation-less String to lowercase
+     * @param s the String to normalize
+     * @return the normalized String
+     */
+    private static String normalize(String s) {
+        StringBuilder retStr = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(Character.isLetter(s.charAt(i))) {
+                retStr.append(s.charAt(i));
+            }
+        }
+        return retStr.toString().toLowerCase();
+    }
 
 
-    
+
+
+
+
+
+
 }
